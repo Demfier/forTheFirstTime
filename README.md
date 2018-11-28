1f64de704703c7713b5b7907ee5ccd15619f033f
demfier <sahu.gaurav719@gmail.com>
2018-11-27 21:35:32 -0500

    Add the dynamic script~~tunes ringing

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 *All the setup I need to do on a fresh install*
 
+**How to execute?**
+
+Run the following in your terminal:
+
+```shell
+chmod +x forTheFirstTime.sh
+./forTheFirstTime.sh your_github_username your_email
+
+```
+
 The script `forTheFirstTime.sh` does the following to my system
 
-1. Setup fish
-2. Install Powerline-fonts
-3. Install solarized theme for terminal
-4. Generate ssh key for aws
-5. Install Sublime Text 3
-6. Ctrl + Shift + P tasks
-    - Install the OneDark theme (the one inspired by Atom) for Sublime
+1. Generate ssh key for aws
+2. Setup fish
+3. Install Powerline-fonts
+4. Install solarized theme for terminal
+5. Setup python3 (pip and aliases)
+6. Install Sublime Text 3
+7. Ctrl + Shift + P tasks
+    - Install the OneDark theme (the material one inspired by Atom) for Sublime
     - Install SublimeLinter
     - Install PEP8 Linter
     - Install pycodestyle
     - Install any other plugin if recall
-7. Copy `sublime_settings.json` to Sublime preferences
-8. Setup git profile in the terminal
+8. Copy `sublime_settings.json` to Sublime preferences
+9. Setup git profile and git aliases in terminal
+10. Set desktop wallpaper (https://financesonline.com/uploads/2014/03/mansion.jpg)
 
-Resources: Refer to Chrome Bookmarks for links to the above utilities
+
+Once everything is done, reboot the system
+
+Resources: Refer to Chrome Bookmarks for links to some the above utilities
--- forTheFirstTime.sh
+++ forTheFirstTime.sh
@@ -1,13 +1,52 @@
-# ToDo: Add code to perform the following tasks
+#!/bin/bash
 
-# Setup fish
+# The above magical comment is important if we want to run the script on a
+# different terminal environment such as fish
 
-# Install Powerline-fonts
-
-# Install solarized theme for terminal
+# TODO: Make python3 default (basically, dynamically add the following aliases to
+# ~/.bashrc and ~/.config/fish/config.fish:
+# alias py='python3'
+# alias python='python3'
+# alias pyhton='python3'
 
 # Generate the ssh key print
+ssh-keygen -t rsa -C "$2"
+
+# Install Dark Solarized theme for terminal
+cd
+wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+mv dircolors.ansi-dark .dircolors
+eval `dircolors ~/.dircolors`
+sudo apt-get install git-core
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./set_dark.sh
+cd
+
+# Install Powerline-fonts
+sudo apt-get install fonts-powerline
+
+# Setup fish and omf
+sudo apt install curl
+sudo apt install git
+sudo apt install fish
+curl -L https://get.oh-my.fish | fish
+omf install agnoster
+chsh -s `which fish`  # set fish as the default terminal
+
 
 # Install Sublime Text 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 
-# Setup git profile in the terminal
+# Setup git profile and aliases in the terminal
+git config --global user.name "$1"
+git config --global user.email "$2"
+git config --global alias.co checkout
+git config --global alias.st status
+
+# Install pip3
+sudo apt install python3-pip
+pip install -U pip  # upgrade pip
